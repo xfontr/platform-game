c343beaa2e29646062973349a3706232b8818ab2
Xifré Font <xifre.font.97@gmail.com>
2025-10-05 19:58:22 +0200

    style(2): Style canvas to fill entire container

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import "./style.css";
 import Canvas from "./shared/canvas/Canvas";
 
-const canvasElement = document.querySelector("canvas#canvas");
+const canvasElement =
+  document.querySelector<HTMLCanvasElement>("canvas#canvas");
 
 new Canvas(canvasElement);
