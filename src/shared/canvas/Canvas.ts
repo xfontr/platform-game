a81f3742b0a7ce480c737749b2831bd2f5ad765b
Xifré Font <xifre.font.97@gmail.com>
2025-10-05 19:58:04 +0200

    feat(2): Observe resizing

--- src/shared/canvas/Canvas.ts
+++ src/shared/canvas/Canvas.ts
@@ -1,14 +1,43 @@
 import CanvasGameError from "./CanvasGameError";
 
-class Canvas {
-  public element: HTMLCanvasElement;
+type ResizeCallback = (element: HTMLCanvasElement) => void;
 
-  constructor(element?: HTMLCanvasElement) {
+class Canvas {
+  private resizeObserver: ResizeObserver | undefined;
+
+  public element: HTMLCanvasElement;
+  public contentRect: DOMRectReadOnly;
+
+  private resizeCallbackStack: ResizeCallback[] = [];
+
+  constructor(element?: HTMLCanvasElement | null) {
     if (!element) {
       throw new CanvasGameError("not-found");
     }
 
     this.element = element;
+    this.contentRect = element.getBoundingClientRect();
+
+    this.observeResize(this.element);
+  }
+
+  private observeResize<T extends Element>(element: T) {
+    this.resizeObserver = new ResizeObserver(([element]) => {
+      this.contentRect = element.contentRect;
+      this.resizeCallbackStack.forEach((callback) => {
+        callback(this.element);
+      });
+    });
+
+    this.resizeObserver.observe(element);
+  }
+
+  onResize(callback: ResizeCallback) {
+    this.resizeCallbackStack.push(callback);
+  }
+
+  onDestroy() {
+    this.resizeObserver?.disconnect();
   }
 }
 
