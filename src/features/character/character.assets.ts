fb188fcb497792c9905fb0805f0b055878216385
Xifré Font <xifre.font.97@gmail.com>
2025-10-10 19:32:06 +0200

    wip(4): Add first animated test

---
+++ src/features/character/character.assets.ts
@@ -0,0 +1,3 @@
+import Assets from "../../shared/assets/Assets";
+
+export const character = new Assets("character").add(2, "walk");
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,36 @@
-import { Application } from "pixi.js";
+import "./style.css";
+import { AnimatedSprite, Application, Assets } from "pixi.js";
 import app from "./shared/app";
+import AssetsManifest from "./shared/assets/AssetsManifest";
+import { character } from "./features/character/character.assets";
+
+const manifest = new AssetsManifest().add("game", character);
+console.log(character.get());
 
 (async () => {
   await app.init(new Application());
+  await manifest.init();
+
+  await Assets.loadBundle("game");
+
+  const canvas = app.get();
+
+  const walkTextures = [
+    Assets.get("0.character.walk"),
+    Assets.get("1.character.walk"),
+  ];
+
+  // 4. Create an AnimatedSprite
+  const character = new AnimatedSprite(walkTextures);
+
+  // 5. Set animation speed and start it
+  character.animationSpeed = 0.1; // speed (0.1 is slow, 1 is fast)
+  character.play();
+
+  // 6. Position the character and add to stage
+  character.x = canvas.canvas.width / 2;
+  character.y = canvas.canvas.height / 2;
+  character.anchor.set(0.5);
+
+  canvas.stage.addChild(character);
 })();
