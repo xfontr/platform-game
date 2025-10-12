6968304abe7b1324ddb264496e64fff21f905eaa
Xifré Font <xifre.font.97@gmail.com>
2025-10-12 17:15:38 +0200

    refactor(8): Update demo

--- src/main.ts
+++ src/main.ts
@@ -2,62 +2,59 @@ import "./style.css";
 import { Application, Assets, Ticker } from "pixi.js";
 import app from "./shared/app";
 import AssetsManifest from "./shared/assets/AssetsManifest";
-import heroInstance from "./entities/hero";
+import hero from "./entities/hero";
 
-const manifest = new AssetsManifest().add("game", heroInstance.assets!);
+const manifest = new AssetsManifest().add("game", hero.assets!);
 
 (async () => {
   await app.init(new Application());
+
   Assets.init({ manifest: manifest.get() });
+
   await Assets.loadBundle("game");
 
   const canvas = app.get();
 
-  heroInstance.state.x = canvas.canvas.width / 2;
-  heroInstance.state.y = canvas.canvas.height / 2;
+  hero.state.x = canvas.canvas.width / 2;
+  hero.state.y = canvas.canvas.height / 2;
 
-  const hero = heroInstance.animate(heroInstance);
-  canvas.stage.addChild(hero.sprite!);
+  hero.startAnimation();
+  const { sprite } = hero.animation!;
+  canvas.stage.addChild(sprite!);
 
   const keys = new Set<string>();
 
   const moveHero = () => {
     const isRunning = keys.has("ShiftLeft") || keys.has("ShiftRight");
-    if (isRunning) heroInstance.run();
-    if (!isRunning) heroInstance.walk();
+    if (isRunning) hero.run();
+    if (!isRunning) hero.walk();
 
     // Horizontal movement
     if (keys.has("ArrowRight") || keys.has("KeyD")) {
-      heroInstance.moveX(1);
-      hero.sprite!.scale.x = Math.abs(hero.sprite!.scale.x); // face right
+      hero.moveX(1);
+      hero.mirror(false);
     }
 
     if (keys.has("ArrowLeft") || keys.has("KeyA")) {
-      heroInstance.moveX(-1);
-      hero.sprite!.scale.x = -Math.abs(hero.sprite!.scale.x); // face left (mirrored)
+      hero.moveX(-1);
+      hero.mirror(true);
     }
 
     // Vertical movement
-    if (keys.has("ArrowUp") || keys.has("KeyW")) heroInstance.moveY(-1);
-    if (keys.has("ArrowDown") || keys.has("KeyS")) heroInstance.moveY(1);
+    if (keys.has("ArrowUp") || keys.has("KeyW")) hero.moveY(-1);
+    if (keys.has("ArrowDown") || keys.has("KeyS")) hero.moveY(1);
   };
 
   const ticker = new Ticker().add(moveHero);
 
   const startWalking = () => {
-    if (!heroInstance.isState("walk")) {
-      heroInstance.setState("walk");
-      hero.sprite!.play();
-    }
+    hero.setModifier("walk");
     if (!ticker.started) ticker.start();
   };
 
   const stopWalking = () => {
     ticker.stop();
-    if (!heroInstance.isState("default")) {
-      heroInstance.setState("default");
-      hero.sprite!.play();
-    }
+    hero.setModifier("default");
   };
 
   window.addEventListener("keydown", (e) => {
