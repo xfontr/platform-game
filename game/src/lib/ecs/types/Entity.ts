aed257b80b2de70c238e5473a625e0b6aa677810
Xifré Font <xifre.font.97@gmail.com>
2025-10-17 19:27:16 +0200

    feat(10): System class

--- game/src/lib/ecs/types/Entity.ts
+++ game/src/lib/ecs/types/Entity.ts
@@ -1,7 +1,9 @@
 import type Component from "../Component";
+import type { Flags } from "./Flags";
 
-export interface Entity {
+export interface Entity extends Flags {
   add<T extends Component>(component: T): this;
   has<T extends Component>(component: T): boolean;
   get<T extends Component>(component: T): T;
+  delete<T extends Component>(component: T): void;
 }
